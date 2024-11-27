1d1a6b165952874ef4c856b6a7596781936e4dd5
Alfridus Elman <alfridusferi@gmail.com>
2024-11-28 01:35:55 +0700

    add firebase

--- src/services/loadModel.js
+++ src/services/loadModel.js
@@ -1,5 +1,17 @@
+// const tf = require("@tensorflow/tfjs-node");
+// async function loadModel() {
+//   return tf.loadGraphModel(process.env.MODEL_URL);
+// }
+// module.exports = loadModel;
+
 const tf = require("@tensorflow/tfjs-node");
+require("dotenv").config();
+
 async function loadModel() {
-  return tf.loadGraphModel(process.env.MODEL_URL);
+  const modelUrl = process.env.MODEL_URL;
+  const model = await tf.loadLayersModel(modelUrl);
+  console.log("Model loaded successfully from:", modelUrl);
+  return model;
 }
+
 module.exports = loadModel;
--- src/services/storeData.js
+++ src/services/storeData.js
@@ -1,10 +1,28 @@
-const { Firestore } = require("@google-cloud/firestore");
+const admin = require("firebase-admin");
+require("dotenv").config();
+
+// Inisialisasi Firebase Admin SDK dengan menggunakan variabel dari .env
+admin.initializeApp({
+  credential: admin.credential.cert({
+    projectId: process.env.FIREBASE_PROJECT_ID,
+    privateKey: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, "\n"), // Mengganti escape character \n dengan newline
+    clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+  }),
+});
+
+const db = admin.firestore();
 
 async function storeData(id, data) {
-  const db = new Firestore();
+  // Menyimpan data ke Firestore
+  const docRef = db.collection("predictions").doc(id);
 
-  const predictCollection = db.collection("prediction");
-  return predictCollection.doc(id).set(data);
+  try {
+    await docRef.set(data);
+    console.log(`Document written with ID: ${id}`);
+  } catch (error) {
+    console.error("Error saving document: ", error);
+    throw new Error("Terjadi kesalahan saat menyimpan data ke Firestore");
+  }
 }
 
 module.exports = storeData;
